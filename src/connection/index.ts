b41719bcd8c7ce98b9743ab2c1d9a2a92063cd5e
kboysm <lishal12@naver.com>
2021-02-03 18:36:25 +0900

    new createConnectionOptions

--- src/connection/index.ts
+++ src/connection/index.ts
@@ -6,7 +6,20 @@ let connection: Connection
 const connectDB = async () => {
     const connectionOptions = await getConnectionOptions();
     connection = await createConnection({
-        ...connectionOptions,
+        "type": "mysql",
+        "host": process.env.DB_HOST,
+        "port": 3306,
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "synchronize": false,
+        "logging": true,
+        "entities": [
+            process.env.NODE_ENV === 'development' ? "src/entity/**/*.*" : "dist/entity/**/*.*"
+        ],
+        "migrations": [
+            "dist/migration/**/*.js"
+        ],
         name: 'default'
     });
     return connection;
