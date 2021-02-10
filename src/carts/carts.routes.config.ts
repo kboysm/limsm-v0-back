4342b488f8561794bac8cf781751dd7cc0197b83
kboysm <lishal12@naver.com>
2021-02-11 03:58:07 +0900

    add cart item

--- src/carts/carts.routes.config.ts
+++ src/carts/carts.routes.config.ts
@@ -14,8 +14,6 @@ export class CartsRoutes extends CommonRoutesConfig {
         this.app.route('/carts/:cartId')
             .get( async (req: express.Request, res: express.Response) => {
                 const cartId = req.params.cartId;
-                const productId = req.params.productId;
-                console.log(cartId , productId)
                 try{   
                     const cart = await getConnection()
                         .getRepository(Carts)
@@ -23,33 +21,45 @@ export class CartsRoutes extends CommonRoutesConfig {
                         .leftJoinAndSelect("carts.cartProduct", "cartproduct")
                         .where("carts.id = :id", { id: cartId })
                         .getOne();
-                //     const p1 = await getConnection()
-                //     .getRepository(Product)
-                //     .createQueryBuilder("product")
-                //     .where("product.id = :id", { id: productId })
-                //     .getOne();
-                // const cartP = new CartProduct();
-                // cartP.imgUrl = p1.imgUrl
-                // cartP.name = p1.name
-                // cartP.description = p1.description
-                // cartP.quantity = p1.quantity
-                // cartP.grade = p1.grade
-                // cartP.salesQuantity = p1.salesQuantity
-                // cartP.price = p1.price
-                // cartP.createdAt = new Date();
-                // cartP.updatedAt = new Date();
-                // cartP.purchaseQuantity = 1;
-                // await getConnection().manager.save(cartP);
-                // cart.cartProduct = [cartP];
-                // await getConnection().manager.save(cart);
-                        // const cart = await  getConnection().getRepository(Carts).find();
                         res.status(200).send([cart])//DB 생성 후 유저 추가 로직
                 } catch(e) {
                     res.status(200).send(e)//DB 생성 후 유저 추가 로직
                 }
             })
-            .post( (req: express.Request, res: express.Response) => {
-                res.status(200).send( `List Of Users `) //DB 생성 후 유저 추가 로직
+            .post( async (req: express.Request, res: express.Response) => {
+                const { id , imgUrl , name , description , quantity , grade , salesQuantity , price } = req.body
+                try{
+
+                const cart = await getConnection()
+                        .getRepository(Carts)
+                        .createQueryBuilder("carts")
+                        .leftJoinAndSelect("carts.cartProduct", "cartproduct")
+                        .where("carts.id = :id", { id: req.params.cartId })
+                        .getOne();
+                const cartP = new CartProduct();
+                cartP.imgUrl = imgUrl
+                cartP.name = name
+                cartP.description = description
+                cartP.quantity = quantity
+                cartP.grade = grade
+                cartP.salesQuantity = salesQuantity
+                cartP.price = price
+                cartP.createdAt = new Date();
+                cartP.updatedAt = new Date();
+                cartP.purchaseQuantity = 1;
+                await getConnection().manager.save(cartP);
+                if(!cart.cartProduct){
+                    cart.cartProduct = [cartP];
+                }else {
+                    cart.cartProduct.push(cartP);
+                }
+                await getConnection().manager.save(cart);
+                        // const cart = await  getConnection().getRepository(Carts).find();
+                    res.status(200).send( `장바구니 추가 `) //DB 생성 후 유저 추가 로직
+                }
+                catch( e ) {
+                    res.status(500).send( `장바구니 추가 실패 `) //DB 생성 후 유저 추가 로직
+                }
             })
 
             return this.app;
